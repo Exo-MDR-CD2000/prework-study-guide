c21a72ab2fc6c1e53e0977d1247dc83c1222e655
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-06-02 18:09:29 -0700

    comments added to explain code

--- assets/script.js
+++ assets/script.js
@@ -33,13 +33,19 @@ for (var x = 0; x < shapes.length; x++) {
 }
 */
 
+// Code above were examples to show 'for loops'
+
 /*
 listTopics()
 selectTopic()
 */
+
+// Lines 39-40 must be removed or commented on so that the rest of the code can be properly displayed in the web browser console log. The course was a bit unclear on that.
 
 console.log('Here are the topics we learned through Prework:');
 listTopics();
 
 console.log('Which topic should we study first?');
 selectTopic();
+
+// All of the listed code (not included the commented ones) displays the topics learned in the prework within the console log. It also randomizes which topic to be studied on with each refresh of the page
