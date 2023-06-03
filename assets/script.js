99d7084e6facc7fbf928d3890c82d48737a97862
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-06-02 17:35:32 -0700

    Hid unused for loop code

--- assets/script.js
+++ assets/script.js
@@ -20,6 +20,7 @@ function selectTopic() {
  }
 }
 
+/*
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 for (var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
@@ -29,3 +30,4 @@ var shapes = ["triangle", "square", "pentagon", "circle"];
 for (var x = 0; x < shapes.length; x++) {
     console.log(shapes[x]);
 }
+*/
